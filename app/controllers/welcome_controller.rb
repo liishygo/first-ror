ef5811eda83cd02a66862454bc85ff8d0da3b69c
liishygo <liishygo@gmail.com>
2010-07-09 18:07:09 +0800

    add more welcome message

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def say
-    render :text => "Hello world!"
+    render :text => "Hello world! This is my first ROR projcet."
   end
   
   def index
